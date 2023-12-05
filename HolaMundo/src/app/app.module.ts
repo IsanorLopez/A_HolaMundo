f1aaafc8eaff1de66cc93836b2ea61990f709f8f
IsanorLopez <isanoralejandro@gmail.com>
2023-12-05 00:47:50 -0700

    Feat: Implementacion de modulos

--- HolaMundo/src/app/app.module.ts
+++ HolaMundo/src/app/app.module.ts
@@ -4,19 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
+    HeroesModule,
     BrowserModule,
+    CounterModule,
     AppRoutingModule
   ],
   providers: [],
