ebffdb786523fd3a6a4f195f40a2ac17a4f9ca14
Isanor Lopez <isanor.lopez@coppel.com>
2023-12-05 17:11:12 -0700

    Feat: Centralizacion de componentes character y list

--- HolaMundo/src/app/dbz/dbz.module.ts
+++ HolaMundo/src/app/dbz/dbz.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/mainpage.component';
+import { ListComponent } from './components/list/list.component';
+import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    ListComponent,
+    MainPageComponent,
+    CharacterComponent
   ],
   imports: [
     CommonModule
