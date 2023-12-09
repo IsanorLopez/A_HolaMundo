99dd130ad60fd2d193ffb56b3fe9df8635fe5a96
IsanorLopez <isanoralejandro@gmail.com>
2023-12-09 14:40:46 -0700

    Feat: Servico para manejar los datos

--- HolaMundo/src/app/dbz/pages/mainpage.component.ts
+++ HolaMundo/src/app/dbz/pages/mainpage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-mainpage',
@@ -8,19 +9,6 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [
-    {name: 'Krilling', power: 1000},
-    {name: 'Goku', power: 9500},
-    {name: 'Vegeta', power: 7000}
-  ];
-
-  onNewCharacter(character: Character):void {
-    console.log(character);
-    this.characters.push(character);
-  }
-
-  onDeleteCharacter(index: number): void {
-    this.characters.splice(index,1);
-  }
+  constructor( public dbzService: DbzService ){}
 
 }
---
+++ HolaMundo/src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+
+  public characters: Character[] = [
+    {name: 'Krilling', power: 1000},
+    {name: 'Goku', power: 9500},
+    {name: 'Vegeta', power: 7000}
+  ];
+
+  onNewCharacter(character: Character):void {
+    console.log(character);
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index: number): void {
+    this.characters.splice(index,1);
+  }
+
+}
